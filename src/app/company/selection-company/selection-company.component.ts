4e64f061af6fc5b0790bb9ace4d1e54da32a38c7
natacharome <natacha-rome@hotmail.fr>
2019-08-17 23:25:16 +0200

    ajout bdd selection company

--- src/app/company/selection-company/selection-company.component.ts
+++ src/app/company/selection-company/selection-company.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { interval } from 'rxjs';
+import { User } from 'src/app/models/User.model';
+import { Selection } from 'src/app/models/Selection.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-selection-company',
@@ -7,9 +11,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectionCompanyComponent implements OnInit {
 
-  constructor() { }
+    click(event: Event): void {
+      console.log("hello");
+
+      const button = event.target as HTMLButtonElement;
+
+      button.style.backgroundColor= 'yellow';
+    }
+      
+    
+
+    //Lists
+    selectionList: Array<Selection> ={} as Array<Selection>;
+    userList : Array<User> = {} as Array<User>;
+
+    //Variables
+    private selectionListLoaded = false;
+    private userListLoaded = false; 
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
+    this.getAll()
+    interval(300000)
+    .subscribe(
+      () => this.getAll()
+    );
+  }
+  getAll() {
+    this.getSelectionList();
+    this.getUserList();
+  }
+
+  getSelectionList(): void {
+    this.http.get<Array<Selection>>('http://pingjob.herokuapp.com/selection/list')
+      // .pipe(map(data => data))
+      .subscribe(
+        (selectionList:Array<Selection>) => {
+          this.selectionList=selectionList;
+          this.selectionListLoaded = true;
+        },
+        err => {
+          console.log(err);
+        }
+      );
+  }
+
+  getUserList(){
+    this.http.get<Array<User>>('hhttp://pingjob.herokuapp.com/user/list')
+    // .pipe(map(data => data))
+    .subscribe(
+      (userList:Array<User>) => {
+        this.userList=userList;
+        this.userListLoaded = true;
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
 }
