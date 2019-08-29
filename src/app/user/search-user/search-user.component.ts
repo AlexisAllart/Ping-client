cf1d1e8907359657430db8af45b887766f751664
AlexisAllart <alexis.allart@gmail.com>
2019-08-29 22:09:39 +0200

    dynamic markers + geolocation

--- src/app/user/search-user/search-user.component.ts
+++ src/app/user/search-user/search-user.component.ts
@@ -11,77 +11,95 @@ declare let L;
   styleUrls: ['./search-user.component.scss']
 })
 export class SearchUserComponent implements OnInit {
-  
+
   // Icons 
   private defaultIcon = L.icon({
     iconUrl: '../../../assets/leaflet/images/marker-icon.png',
     //pop up quand on clique, le met juste au dessus
-    popupAnchor: [13,-2],
+    popupAnchor: [13, -2],
+    shadowUrl: '../../../assets/leaflet/images/marker-shadow.png',
+  });
+  private redIcon = L.icon({
+    iconUrl: '../../../assets/leaflet/images/marker-icon-red.png',
+    //pop up quand on clique, le met juste au dessus
+    popupAnchor: [13, -2],
     shadowUrl: '../../../assets/leaflet/images/marker-shadow.png',
   });
 
   // Map Variables
   private map;
-  private markers=[];
+  private markers = [];
   private markerLayer;
+
+  private filteredArray = [];
+
+  private geolocationPosition;
 
   constructor(
     private route: ActivatedRoute,
     private authService: AuthService
-    ) { }
+  ) { }
 
-  folderObjs=this.route.snapshot.data.offerList;
-  search='';
-  
+  private offersWithKeyWords = this.route.snapshot.data.offerList;
+  private search = '';
+
   ngOnInit() {
-    // JSON.parse(localStorage.getItem('id')) !== null?this.userService.preloadUser():'';
-
-    this.initMap();
-    for (let i=0;i<this.route.snapshot.data.offerList.length;i++) {
-      this.folderObjs[i].keyWords =
-      this.route.snapshot.data.keyWordList[this.route.snapshot.data.offerList[i].keyWordOne_id-1].name+
-      this.route.snapshot.data.keyWordList[this.route.snapshot.data.offerList[i].keyWordTwo_id-1].name+
-      this.route.snapshot.data.keyWordList[this.route.snapshot.data.offerList[i].keyWordThree_id-1].name
+    for (let i = 0; i < this.route.snapshot.data.offerList.length; i++) {
+      this.offersWithKeyWords[i].keyWords =
+        this.route.snapshot.data.keyWordList[this.route.snapshot.data.offerList[i].keyWordOne_id - 1].name +
+        this.route.snapshot.data.keyWordList[this.route.snapshot.data.offerList[i].keyWordTwo_id - 1].name +
+        this.route.snapshot.data.keyWordList[this.route.snapshot.data.offerList[i].keyWordThree_id - 1].name
     }
-    console.log(this.folderObjs);
+    this.filteredArray = this.offersWithKeyWords.filter((v) => v.keyWords.toLowerCase().indexOf(this.search.toLowerCase()) > -1);
+    if (window.navigator && window.navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition(
+        position => {
+          this.geolocationPosition = position;
+          this.initMap();
+        },
+        error => {
+          this.initMap();
+          switch (error.code) {
+            case 1:
+              alert('ERREUR Localisation : Permission refusée');
+              break;
+            case 2:
+              alert('ERREUR Localisation : Impossible de trouver votre position');
+              break;
+            case 3:
+              alert('ERREUR Localisation : Aucune réponse du navigateur');
+              break;
+          }
+        }
+      );
+    };
   }
 
-  ///////////////////////////////////////////////
-
-  
-
-  
-  // [{
-  //   name:'flo mdr',
-  //   size:'24 ans'
-  // },{
-  //   name:'wahiba <3',
-  //   size:'36 ans'
-  // },{
-  //   name:'alex^^',
-  //   size:'33 ans'
-  // },{
-  //   name:'natacha <3',
-  //   size:'25 ans'
-  // }]
-
-  ////////////////////////////////////////////////
-  
-  initMap() {
-    this.map = L.map('map').setView([50.6342, 3.02046], 15);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(this.map)
+  change() {
+    this.filteredArray = this.offersWithKeyWords.filter((v) => v.keyWords.toLowerCase().indexOf(this.search.toLowerCase()) > -1);
     this.populateMarkers();
   }
-  
+
+  initMap() {
+    let lat,lon;
+    this.geolocationPosition.coords.latitude ? lat = this.geolocationPosition.coords.latitude : lat = 50.6342;
+    this.geolocationPosition.coords.longitude ? lon = this.geolocationPosition.coords.longitude : lon = 3.02046;
+    this.map = L.map('map').setView([lat, lon], 15);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+    this.geolocationPosition.coords.latitude ? L.marker([this.geolocationPosition.coords.latitude, this.geolocationPosition.coords.longitude], { icon: this.redIcon }).bindPopup("Vous êtes ici !").addTo(this.map):'';
+    this.populateMarkers();
+  }
+
   //Poser les markers sur la map
   populateMarkers(): void {
-    this.markerLayer?this.markerLayer.clearLayers():'';
-    for(let i=0;i<this.route.snapshot.data.offerList.length;i++){
-      this.markers[i]=L.marker([this.route.snapshot.data.offerList[i].latitude,this.route.snapshot.data.offerList[i].longitude],{icon:this.defaultIcon}).bindPopup(this.route.snapshot.data.offerList[i].title+" : "+this.route.snapshot.data.offerList[i].description);
+    this.markers = [];
+    this.markerLayer ? this.markerLayer.clearLayers() : '';
+    for (let i = 0; i < this.filteredArray.length; i++) {
+      this.markers[i] = L.marker([this.filteredArray[i].latitude, this.filteredArray[i].longitude], { icon: this.defaultIcon }).bindPopup(this.filteredArray[i].title + " : " + this.filteredArray[i].description);
     }
-    this.markerLayer=L.layerGroup(this.markers);
+    this.markerLayer = L.layerGroup(this.markers);
     this.map.addLayer(this.markerLayer);
   }
 }
